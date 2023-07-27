01b94aeff887d6edef3699059abecf4869c04e88
AldoLamberto <aldowhasap@gmail.com>
2023-07-27 18:52:12 -0300

    modificaciones en volver al index del formulario

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const puerto = process.env.Port || 3030;
+const puerto = process.env.Port || 3031;
 
 app.use(express.json());
 app.use("/", express.static(__dirname + "/public"));
@@ -23,3 +23,24 @@ app.get("/login", (req, res) => {
 app.listen(puerto, () => {
   console.log("Aplicación esuchando en puerto 3031");
 });
+
+// Formulario Login
+
+app.get("/login", (req,res) => {
+ let htmlPath = path.resolve(__dirname, "/views/login.html");
+  res.sendFile(htmlPath)
+ })
+
+ app.post("/login", (req, res) => {
+   res.send("LLEGO EL FORMULARIO DE LOGIN")
+ })
+
+// Formulario Register
+app.get("/register", (req,res) => {
+let htmlPath = path.resolve(__dirname, "/views/register.html");
+  res.sendFile(htmlPath)
+})
+
+app.post("/register", (req, res) => {
+  res.send("REGISTRO COMPLETADO");
+   })
